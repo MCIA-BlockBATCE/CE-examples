9a6b673e37ce5c4cae7fc8f707f4f45368bba900
unknown <albert.llufriu@estudiantat.upc.edu>
2024-09-14 18:07:53 +0200

    Tasca 1 i 2

---
+++ _slides_scripts/task 1/ANFIS_RNN_ARIMA.m
@@ -0,0 +1,254 @@
+clear all
+close all
+
+%% ANFIS
+% 1h prediction (4 samples)
+load ConsProfileExample.mat
+data = ConsProfileExample;
+
+% inputs based on previous data
+consumption_dif4 = zeros(672,1);
+previous_day = zeros(672,1);
+previous_sample = zeros(672,1);
+    
+    for t=1:672 
+        
+        % Differential consumption from the last 4 hours
+        if t>=(4*4)+1
+            consumption_dif4(t,1)=(data(t,1)-data(t-4*4,1));
+        end
+    
+        % Consumption of current hour the previous day
+        if t>=(4*24)+1
+            previous_day(t,1)=(data(t-(4*24)));
+        end
+
+        % Consumption of previous sample
+        if t>=2
+            previous_sample(t,1) = data(t-1,1);
+        end
+    
+    end
+
+% group inputs and outputs into I/O matrix
+% IO_matrix will be used from i=97 to i=672
+IO_matrix(:,1) = previous_sample;
+IO_matrix(:,2) = previous_day;
+IO_matrix(:,3) = consumption_dif4;
+IO_matrix(:,4) = data; % Output vector
+
+% train/test split
+IO_matrix_training = IO_matrix(97:600,:);
+IO_matrix_testing = IO_matrix(601:672,:);
+
+% ANFIS
+opt=anfisOptions('EpochNumber',5);
+fis = anfis(IO_matrix_training,opt);
+predicted_data_ANFIS_1H = evalfis(fis,IO_matrix_testing(:,1:3));
+RMS_1h_ANFIS = rms(predicted_data_ANFIS_1H  - data(601:672,1));
+
+% test data and prediction comparison
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_1h_ANFIS};
+
+figure(1)
+plot(1:72,predicted_data_ANFIS_1H ,1:72,data(601:672,1))
+title("ANFIS model with forecasting horizon: 1 hour (4 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+clear IO_matrix
+
+% 3h prediction (12 samples)
+
+% use previous consumption data and inputs
+
+% regroup into input/output matrix changing output vector
+% IO_matrix will be used from i=97 to i=664
+IO_matrix(:,1) = previous_sample(1:672-2*4,1);
+IO_matrix(:,2) = previous_day(1:672-2*4,1);
+IO_matrix(:,3) = consumption_dif4(1:672-2*4,1);
+IO_matrix(:,4) = data(1+2*4:672,1); % Output vector
+
+% train/test split (about 70/30)
+IO_matrix_training = IO_matrix(97:592,:);
+IO_matrix_testing = IO_matrix(593:664,:);
+
+% ANFIS
+opt=anfisOptions('EpochNumber',5);
+fis = anfis(IO_matrix_training,opt);
+predicted_data_ANFIS_3h = evalfis(fis,IO_matrix_testing(:,1:3));
+RMS_3h_ANFIS = rms(predicted_data_ANFIS_3h - data(593:664,1));
+
+% test data and prediction comparison
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_3h_ANFIS};
+figure(2)
+plot(1:72,predicted_data_ANFIS_3h,1:72,data(593:664,1))
+title("ANFIS model with forecasting horizon: 3 hours (12 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+%% Recurrent neural net prediction 
+% 1h prediction (4 steps)
+% Load Consumption data
+load ConsProfileExample.mat
+
+XTrain = ConsProfileExample(1:596,1);
+TTrain = ConsProfileExample(5:600,1);
+
+XTest = ConsProfileExample(597:668,1);
+TTest = ConsProfileExample(601:672,1);
+
+layers = [
+    sequenceInputLayer(1)
+    lstmLayer(128)
+    fullyConnectedLayer(1)];
+
+options = trainingOptions("adam", ...
+    MaxEpochs=1000, ...
+    SequencePaddingDirection="left", ...
+    Shuffle="every-epoch", ...
+    Plots="training-progress", ...
+    Verbose=false);
+
+net = trainnet(XTrain,TTrain,layers,"mse",options);
+
+Tpredict = predict(net,XTest);
+
+RMS_1h_RNN = rms(Tpredict-TTest);
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_1h_RNN};
+
+figure(3)
+plot(1:72,Tpredict,1:72,TTest)
+title("RNN model with forecasting horizon: 1 hour (4 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+% 3h prediction (12 steps)
+% Load Consumption data
+load ConsProfileExample.mat
+
+XTrain = ConsProfileExample(1:588,1);
+TTrain = ConsProfileExample(13:600,1);
+
+XTest = ConsProfileExample(589:660,1);
+TTest = ConsProfileExample(601:672,1);
+
+layers = [
+    sequenceInputLayer(1)
+    lstmLayer(128)
+    fullyConnectedLayer(1)];
+
+options = trainingOptions("adam", ...
+    MaxEpochs=1000, ...
+    SequencePaddingDirection="left", ...
+    Shuffle="every-epoch", ...
+    Plots="training-progress", ...
+    Verbose=false);
+
+net = trainnet(XTrain,TTrain,layers,"mse",options);
+
+Tpredict = predict(net,XTest);
+
+RMS_3h_RNN = rms(Tpredict-TTest);
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_3h_RNN};
+
+figure(4)
+plot(1:72,Tpredict,1:72,TTest)
+title("RNN model with forecasting horizon: 3 hour (12 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+%% Autoregressive integrated moving average prediction
+% 1h prediction (4 samples)
+load ConsProfileExample.mat
+
+% Create model with 1 nonseasonal autoregressive polynomial degree, 1
+% nonseasonal integration degree and 1 nonseasonal moving average
+% polynomial degree
+Mdl = arima(1,1,1);
+
+% Fit model to data
+idxpre = 1:Mdl.P;
+idxest = (Mdl.P + 1):597;
+EstMdl = estimate(Mdl,ConsProfileExample(idxest),...
+    'Y0',ConsProfileExample(idxpre));
+
+
+% Forecast consumption into 4 sample horizon using estimated model
+% Last two observations in estimation data are specified as presample.
+yf0 = ConsProfileExample(idxest(end - 1:end));
+for t=1:72
+    yf = forecast(EstMdl,4,yf0);
+    ypred(t,1) = yf(4,1); % We keep 4th step prediction
+    idxest = (Mdl.P + 1):597+t;
+    yf0 = ConsProfileExample(idxest(end - 1:end));
+end
+
+RMS_1h_ARIMA = rms(ypred  - ConsProfileExample(601:672,1));
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_1h_ARIMA};
+
+% Plot of observations and forecast 
+figure(5)
+plot(1:72,ypred,1:72,ConsProfileExample(601:672));
+title("ARIMA model with forecasting horizon: 1 hour (4 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+% 3h prediction (12 samples)
+load ConsProfileExample.mat
+
+% Create model with 1 nonseasonal autoregressive polynomial degree, 1
+% nonseasonal integration degree and 1 nonseasonal moving average
+% polynomial degree
+Mdl = arima(1,1,1);
+
+% Fit model to data
+idxpre = 1:Mdl.P;
+idxest = (Mdl.P + 1):589;
+EstMdl = estimate(Mdl,ConsProfileExample(idxest),...
+    'Y0',ConsProfileExample(idxpre));
+
+
+% Forecast consumption into 12 sample horizon using estimated model
+% Last two observations in estimation data are specified as presample.
+yf0 = ConsProfileExample(idxest(end - 1:end));
+for t=1:72
+    yf = forecast(EstMdl,12,yf0);
+    ypred(t,1) = yf(12,1); % We keep 12th step prediction
+    idxest = (Mdl.P + 1):597+t;
+    yf0 = ConsProfileExample(idxest(end - 1:end));
+end
+
+RMS_3h_ARIMA = rms(ypred  - ConsProfileExample(601:672,1));
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_3h_ARIMA};
+
+% Plot of observations and forecast
+figure(6)
+plot(1:72,ypred,1:72,ConsProfileExample(601:672));
+title("ARIMA model with forecasting horizon: 3 hour (12 steps)")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+
---
+++ _slides_scripts/task 2/ANFIS_predictor_comparison.m
@@ -0,0 +1,148 @@
+clear all
+close all
+
+%% Prepare Inputs
+
+% Load data
+base_data = readmatrix("Dades_iCat_base.xlsx");
+RES3_cons = base_data(1:64216,13);
+temp_data = readmatrix("temperaturas_2022_2023_15minutal.xlsx");
+NumSamples = 64216;
+
+% Set indexes
+hot_test_month = 17373:20348;
+cold_test_month = 32061:35036;
+hot_training_period = [9981:17372 20349:29180 45017:64216-4];
+cold_training_period = [(4*24*7*2+1):9980 29181:32060 35037:45016];
+whole_training_period = [(4*24*7*2+1):17372 20349:32060 35037:64216-4];
+
+% Remove NaNs and apply moving window
+RES3_cons(:,:) = fillmissing(RES3_cons(:,:),'linear');
+RES3_cons=matlab.tall.movingWindow(@mean,15,RES3_cons);
+
+% Time inputs
+instant_day = zeros(NumSamples,1);
+instant_week = zeros(NumSamples,1);
+
+qh=1;
+week_day=7; %2022 started on sunday
+
+for t=1:NumSamples
+    instant_day(t,1)=qh;
+    instant_week(t,1)=week_day;
+    qh=qh+1;
+
+    if qh==97
+        week_day=week_day+1;
+        qh=1;
+    end
+
+    if week_day==8
+        week_day=1;
+    end
+end
+
+% Temperature inputs
+
+temp_data = temp_data(:,6);
+
+% Inputs related to previous consumption
+
+cons_dif4 = zeros(NumSamples,1);
+cons_mean = zeros(NumSamples,1);
+
+for t=1:NumSamples
+
+    % Consumo diferencial respecto a hace 4 horas
+    if t>=(4*4)+1
+        cons_dif4(t,1)=(RES3_cons(t,1)-RES3_cons(t-4*4,1));
+    end
+
+    % Media de consumo de las dos ultimas semanas
+    if t>=(4*24*7*2)+1
+        cons_mean(t,1)=(RES3_cons(t-(4*24*7),1)+RES3_cons(t-(4*24*7*2),1))/2;
+    end
+
+end
+
+%% ANFIS Configuration
+
+% Matrix Inputs/Outputs
+data_ANFIS(:,1)=instant_day(1:NumSamples-4,1);
+data_ANFIS(:,2)=instant_week(1:NumSamples-4,1);
+data_ANFIS(:,3)=RES3_cons(1:NumSamples-4,1); 
+data_ANFIS(:,4)=temp_data(1:NumSamples-4,1);
+data_ANFIS(:,5)=cons_dif4(1:NumSamples-4,1);
+data_ANFIS(:,6)=cons_mean(1:NumSamples-4,1); 
+data_ANFIS(:,7)=RES3_cons(5:NumSamples,1); 
+
+data_ANFIS(:,:) = fillmissing(data_ANFIS(:,:),'linear');
+
+% Options
+opt=anfisOptions('EpochNumber',3);
+
+% Training
+fis_hot = anfis(data_ANFIS(hot_training_period,:),opt);
+fis_cold = anfis(data_ANFIS(cold_training_period,:),opt);
+fis_all = anfis(data_ANFIS(whole_training_period,:),opt);
+
+%% Testing
+
+% Using season predictors
+pred_summer_hotpredictor = evalfis(fis_hot,data_ANFIS(hot_test_month,1:6));
+pred_winter_coldpredictor = evalfis(fis_cold,data_ANFIS(cold_test_month,1:6));
+
+% Using whole data predictor
+pred_summer_whole = evalfis(fis_all,data_ANFIS(hot_test_month,1:6));
+pred_winter_whole = evalfis(fis_all,data_ANFIS(cold_test_month,1:6));
+
+% RMS evaluations
+RMS_summer_hotpredictor = rms(pred_summer_hotpredictor-RES3_cons(hot_test_month));
+RMS_winter_coldpredictor = rms(pred_winter_coldpredictor-RES3_cons(cold_test_month));
+RMS_summer_whole= rms(pred_summer_whole-RES3_cons(hot_test_month));
+RMS_winter_whole = rms(pred_winter_whole-RES3_cons(cold_test_month));
+
+% Plots
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_summer_hotpredictor};
+figure(1)
+plot(1:2976,pred_summer_hotpredictor,1:2976,RES3_cons(hot_test_month,1))
+title("Summer month prediction using high temperatures predictor")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_winter_coldpredictor};
+figure(2)
+plot(1:2976,pred_winter_coldpredictor,1:2976,RES3_cons(cold_test_month,1))
+title("Winter month prediction using low temperatures predictor")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_summer_whole};
+figure(3)
+plot(1:2976,pred_summer_whole,1:2976,RES3_cons(hot_test_month,1))
+title("Summer month prediction using whole year predictor")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+dim = [0.15 0.5 0.5 0.4];
+str = {'RMS' RMS_winter_whole};
+figure(4)
+plot(1:2976,pred_winter_whole,1:2976,RES3_cons(cold_test_month,1))
+title("Winter month prediction using whole year predictor")
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+xlabel("Time [15 min]")
+ylabel("Power consumption [kW]")
+legend("Predicted consumption data","Real consumption data")
+
+% Having different predictors depending on temperature doesn't seem to improve
+% performance of forecasting.
